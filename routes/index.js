caed6b891453c39c70c8a72a18cb440143ed7c38
unknown <yojinakano@gmail.om>
2020-07-28 21:39:02 +0900

    signin

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('login', { title: 'Login画面' });
+  res.render('login', { title: 'login' });
 });
 
 module.exports = router;

775bb7d083bdf1b9d222e6aa91d2845a3faedbb6
syojinaka <yojinakano@gmial.com>
2022-02-26 12:02:20 +0900

    サーバー起動時にDB接続されるように修正

---
+++ util/connectMysql.js
@@ -0,0 +1,23 @@
+const config = require('../config.json').develop
+const mysql = require('mysql');
+
+
+// DB設定読み込み
+const connection = mysql.createConnection({
+    host: config.host,
+    user: config.user,
+    password: config.password,
+    database: config.database
+  })
+  
+  // DB接続
+  connection.connect((err) => {
+    if (err) {
+        console.log('error connecting:' + err.stack);
+        return;
+    }
+    console.log('conneted DB');
+  })
+
+
+  module.exports = connection;
